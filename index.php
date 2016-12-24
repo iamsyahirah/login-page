67f707f707aed3ccee566fdcdc0ae77ca5e1d822
syahirah <syahirahmohammad89@gmail.com>
2016-12-25 02:09:22 +0800

    create css file

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <html>
 <head>
   <title>Welcome to my Website.</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
   <body>
     <h1>Please Login or Register</h1>
+    
 
   </body>
 </html
