d5861e87c91617ef29d6fceffb792440b2e49cb0
syahirah <syahirahmohammad89@gmail.com>
2016-12-26 22:21:15 +0800

    create login form

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Login Below</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+  <body>
+    <h1>Login</h1>
+
+    <form action="login.php" method="POST">
+      <input type="text" placeholder="Enter your email" name="email">
+      <input type="password" placeholder="Enter your password" name="password">
+      <input type="submit">
+    </form>
+
+
+  </body>
+</html
