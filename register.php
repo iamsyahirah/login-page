477b336cb0a5c08a04ee98f3d4b54c0307fe172b
syahirah <syahirahmohammad89@gmail.com>
2016-12-26 22:21:43 +0800

    create register file

---
+++ register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Register Below</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+  <body>
+    <h1>Register</h1>
+
+
+  </body>
+</html
